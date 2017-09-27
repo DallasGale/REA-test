bb344be56b88e830df4cd8357980e1b5148840c5
Dallas Gale <dallas@mylocalbroker.com.au>
2017-09-27 22:03:20 +1000

    imported service to app.component so data can be used

--- rea-app/src/app/app.component.ts
+++ rea-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReaDataService } from './services'; // imported data service
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+
+export class AppComponent implements OnInit {
+    title = 'REA Coding Challenge';
+    data: any;
+
+    // added data service to the constructor
+    constructor(private reaDataService: ReaDataService) { }
+
+    // initialised the service
+    ngOnInit() {
+        this.data = this.reaDataService.getReaData();
+    }
 }
