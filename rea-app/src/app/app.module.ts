a2d4fa9f4e61f7531b8e9987a1473b8cffa8010d
Dallas <dallas@mylocalbroker.com.au>
2017-10-01 08:57:18 +1100

    Flex module
    • To be used for responsive grid layout

--- rea-app/src/app/app.module.ts
+++ rea-app/src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { MaterialModule } from './material.module';
-
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { 
+  ResultsComponent,
+  SavedPropertiesComponent 
+} from './components/';
 
-import { ReaDataService } from './services/'
+import { ReaDataService,  } from './services/'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsComponent,
+    SavedPropertiesComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    FlexLayoutModule,
     MaterialModule,
   ],
   providers: [
