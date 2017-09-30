08f69fd4b852bbd0be786111ff90ae69796262ff
Dallas <dallas@mylocalbroker.com.au>
2017-10-01 08:52:57 +1100

    Saved Properties Component
    • Eventuallt all the saved property code  wll be housed withing this saved-properties.component.

---
+++ rea-app/src/app/components/saved-properties/saved-properties.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { ReaDataService } from '../../services'; // imported data service
+
+@Component({
+    selector: 'app-saved-properties',
+    templateUrl: './saved-properties.component.html',
+    styleUrls: ['./saved-properties.component.scss']
+})
+
+export class SavedPropertiesComponent  {    
+}
