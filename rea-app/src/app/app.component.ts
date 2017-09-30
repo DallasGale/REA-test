89525b757dbe4662928e1eca57b0324a3111950b
Dallas <dallas@mylocalbroker.com.au>
2017-10-01 08:49:51 +1100

    Function added
    • Created 'save' and 'delete' (click) functions.
    • Initialised empty 'savedProperties' array

--- rea-app/src/app/app.component.ts
+++ rea-app/src/app/app.component.ts
@@ -4,17 +4,31 @@ import { ReaDataService } from './services'; // imported data service
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss'],
 })
-
 
 export class AppComponent implements OnInit {
     title = 'REA Coding Challenge';
     data: any;
+    public recentProperty;
+    public savedProperties = [];
+    
+    saveProperty(property) {        
+        console.log(JSON.stringify(property));
+        this.savedProperties.push(property);
+    }
+
+    deleteProperty(property) {
+        console.log("delete property = " + property);
+        var index = this.savedProperties.indexOf(property);
+        if (index >= 0) {
+            this.savedProperties.splice(index, 1);
+        }
+    }
 
     // added data service to the constructor
     constructor(private reaDataService: ReaDataService) { }
-
+    
     // initialised the service
     ngOnInit() {
         this.data = this.reaDataService.getReaData();
